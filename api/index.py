1b5de4963267f504740833c592f60bc237308a98
Dhawal Upadhyay <dhawalupadhyay@Dhawals-MacBook-Air.local>
2026-01-28 19:20:43 +0530

    catching route

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Request
 
 print("FASTAPI BACKEND LOADED")
 app = FastAPI(title="AskDhawal API")
@@ -6,3 +7,10 @@ app = FastAPI(title="AskDhawal API")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def catch_all(full_path: str, request: Request):
+    return [
+        {"path": r.path, "methods": list(r.methods)}
+        for r in app.routes
+    ]
