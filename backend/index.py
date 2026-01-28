0aac509f0f6a1695476b3e1d5017243016a83ba6
Dhawal Upadhyay <dhawalupadhyay@Dhawals-MacBook-Air.local>
2026-01-28 18:50:18 +0530

    fixing sys path

--- backend/index.py
+++ backend/index.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI
 from app.api.chat import router as chat_router
 from fastapi.middleware.cors import CORSMiddleware
